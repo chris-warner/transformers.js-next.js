2b5b2054d361328eb7a02ce9dfaba45ebf632fc7
Christopher Warner <cwarner@concentrichx.com>
2024-03-18 20:19:57 -0400

    feat: analysis

--- transformers.js-next.js/next.config.mjs
+++ transformers.js-next.js/next.config.mjs
@@ -1,4 +1,20 @@
+//next.config.js file is updated to ignore node-specific modules when bundling for the browser
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // (Optional) Export as a static site
+    // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+    output: 'export', // Feel free to modify/remove this option
+
+    // Override the default webpack configuration
+    webpack: (config) => {
+        // See https://webpack.js.org/configuration/resolve/#resolvealias
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "sharp$": false,
+            "onnxruntime-node$": false,
+        }
+        return config;
+    },
+}
 
 export default nextConfig;
